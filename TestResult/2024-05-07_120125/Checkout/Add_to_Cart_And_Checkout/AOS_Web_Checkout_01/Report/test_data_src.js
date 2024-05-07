48549df75843fe298b333e35bbad9af81b1d37f2
YapKahYun <kahyun.yap@appfuxion.com>
2024-05-07 12:05:09 +0800

    Added Sample Test Result

---
+++ TestResult/2024-05-07_120125/Checkout/Add_to_Cart_And_Checkout/AOS_Web_Checkout_01/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\HP\\OneDrive\\Desktop\\UFT Framework Enhanced\\Enhanced-UFT-Framework\\TestResult\\2024-05-07_120125\\Checkout\\Add_to_Cart_And_Checkout\\AOS_Web_Checkout_01',namePairs:[]}]};
---
+++ TestResult/2024-05-07_120125/Contact Us/MoreInfo_ContactUs_Page/AOS_Web_ContactUs_01/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\HP\\OneDrive\\Desktop\\UFT Framework Enhanced\\Enhanced-UFT-Framework\\TestResult\\2024-05-07_120125\\Contact Us\\MoreInfo_ContactUs_Page\\AOS_Web_ContactUs_01',namePairs:[]}]};
